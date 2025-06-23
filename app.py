e6b8a110346dcd821c0d1d7867379bc6e91e13a5
kognitiveweb <reachkiot@gmail.com>
2025-06-23 18:11:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ configure(app, App)
 # ✅ Run with uvicorn if executed directly
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
